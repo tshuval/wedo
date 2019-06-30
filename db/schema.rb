31df20e3f1212e6dd0b0bf376b82ea804f389382
Tsachi Shuval <tsachi.shuval@wework.com>
2019-06-30 17:32:39 +0300

    changed column from 'type' to 'tag_type'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_125707) do
+ActiveRecord::Schema.define(version: 2019_06_30_143020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2019_06_30_125707) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
-    t.string "type", default: "tag", null: false
+    t.string "tag_type", default: "tag", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name"
