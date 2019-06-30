6d911ed741676c608ab8a4ac25a81c632e2502aa
Tsachi Shuval <tsachi.shuval@wework.com>
2019-06-30 16:25:27 +0300

    models and migrations

---
+++ db/migrate/20190630122018_create_join_table_places_tags.rb
@@ -0,0 +1,8 @@
+class CreateJoinTablePlacesTags < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :places, :tags do |t|
+      # t.index [:place_id, :tag_id]
+      t.index [:tag_id, :place_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_30_125707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "places", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.string "address", null: false
+    t.string "website"
+    t.string "phone"
+    t.string "email"
+    t.float "lat"
+    t.float "lon"
+    t.float "average_score", default: 0.0
+    t.boolean "is_active", default: true
+    t.time "sun_open"
+    t.time "sun_close"
+    t.time "mon_open"
+    t.time "mon_close"
+    t.time "tue_open"
+    t.time "tue_close"
+    t.time "wed_open"
+    t.time "wed_close"
+    t.time "thu_open"
+    t.time "thu_close"
+    t.time "fri_open"
+    t.time "fri_close"
+    t.time "sat_open"
+    t.time "sat_close"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "places_tags", id: false, force: :cascade do |t|
+    t.bigint "place_id", null: false
+    t.bigint "tag_id", null: false
+    t.index ["tag_id", "place_id"], name: "index_places_tags_on_tag_id_and_place_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "username", null: false
+    t.text "description"
+    t.integer "score", null: false
+    t.bigint "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_reviews_on_place_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "type", default: "tag", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name"
+  end
+
+  add_foreign_key "reviews", "places"
 end
