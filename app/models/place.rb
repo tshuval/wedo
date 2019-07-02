df3ecc95e355b51a26f3fba478fb5ed5c29efdab
Tsachi Shuval <tsachi.shuval@wework.com>
2019-07-02 10:04:10 +0300

    linting

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,4 @@
 class Place < ApplicationRecord
-	has_and_belongs_to_many :tags
-	has_many :reviews
+  has_and_belongs_to_many :tags
+  has_many :reviews
 end
--- db/migrate/20190630143020_change_column_name.rb
+++ db/migrate/20190630143020_change_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeColumnName < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :tags, :type, :tag_type
+    rename_column :tags, :type, :tag_type
   end
 end
