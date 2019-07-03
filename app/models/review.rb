6794fd8c457c408a6cfc3f033bdb0dbc8ac1c621
Tsachi Shuval <tsachi.shuval@wework.com>
2019-07-03 12:10:58 +0300

    added validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :place
+  validates :score, numericality: {only_integer: true, greater_than: 0, less_than: 6, message: 'Review score must be 1-5'}
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,5 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :places
+  validates :name, length: { in: 2..30, message: 'Tag length must be 2-30 characters'}
+  validates :tag_type, inclusion: {in: ['tag', 'place']}
 end
