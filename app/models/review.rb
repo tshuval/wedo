4b9dc531fad2f0747360bf5deb1165c94742cae2
Tsachi Shuval <tsachi.shuval@wework.com>
2019-07-03 16:28:36 +0300

    linting

--- app/models/review.rb
+++ app/models/review.rb
@@ -13,5 +13,5 @@
 
 class Review < ApplicationRecord
   belongs_to :place
-  validates :score, numericality: {only_integer: true, greater_than: 0, less_than: 6, message: 'Review score must be 1-5'}
+  validates :score, numericality: { only_integer: true, greater_than: 0, less_than: 6, message: 'Review score must be 1-5' }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -11,6 +11,6 @@
 
 class Tag < ApplicationRecord
   has_and_belongs_to_many :places
-  validates :name, length: { in: 2..30, message: 'Tag length must be 2-30 characters'}
-  validates :tag_type, inclusion: {in: ['tag', 'place']}
+  validates :name, length: { in: 2..30, message: 'Tag length must be 2-30 characters' }
+  validates :tag_type, inclusion: { in: %w(tag place) }
 end
