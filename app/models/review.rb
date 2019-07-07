24a6bbbc66ef1423d54475611c4fea7b3ed1a076
Tsachi Shuval <tsachi.shuval@wework.com>
2019-07-07 11:57:54 +0300

    row order

--- app/models/review.rb
+++ app/models/review.rb
@@ -12,6 +12,7 @@
 #
 
 class Review < ApplicationRecord
-  belongs_to :place
   validates :score, numericality: { only_integer: true, greater_than: 0, less_than: 6, message: 'Review score must be 1-5' }
+
+  belongs_to :place
 end
