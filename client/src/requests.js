3478f7ca15abddb404105473eff9b1971dead569
Tsachi Shuval <tsachi.shuval@wework.com>
2019-07-18 14:58:51 +0300

    using async-await for ajax requests

--- client/src/requests.js
+++ client/src/requests.js
@@ -10,18 +10,17 @@ const tzOffset = new Date().getTimezoneOffset() * -60;
 
 export class BackendClient {
   // Get list of tags matching 'q'
-  getTags = (q: string) => (
-    axios.get('/tags?q=' + q)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  getTags = async (q: string) => {
+    try {
+      const response = await axios.get('/tags?q=' + q);
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   // Get a list of places
-  getPlaces = (q?: string, openNow?: boolean) => {
+  getPlaces = async (q?: string, openNow?: boolean) => {
     let params: {
       q?: string,
       open_now?: string,
@@ -30,67 +29,61 @@ export class BackendClient {
     if (q) params['q'] = q;
     if (openNow) params['open_now'] = ''
 
-    axios.get('/places', {params: params})
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    try {
+      const response = await axios.get('/places', {params: params});
+      console.log(response);
+    } catch(error) {
+        console.error(error);
+    }
   }
 
   // Get list of reviews for place
-  getReviews = (placeId: string) => (
-    axios.get('/places/' + placeId + '/reviews')
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  getReviews = async (placeId: string) => {
+    try {
+      const response = await axios.get('/places/' + placeId + '/reviews');
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   // Get a place
-  getPlace = (placeId: string) => (
-    axios.get('/places/' + placeId)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  getPlace = async (placeId: string) => {
+    try {
+      const response = await axios.get('/places/' + placeId);
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   // Create a place
-  createPlace = (placeParams: PlaceProps) => (
-    axios.post('/places/', placeParams)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  createPlace = async (placeParams: PlaceProps) => {
+    try {
+      const response = await axios.post('/places/', placeParams);
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   // Update a place
-  updatePlace = (placeId: string, placeParams: PlaceProps) => (
-    axios.put('/places/' + placeId, placeParams)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  updatePlace = async (placeId: string, placeParams: PlaceProps) => {
+    try {
+      const response = await axios.put('/places/' + placeId, placeParams);
+      console.log(response);
+    } catch(error) {
+      console.error(error);
+    }
+  }
 
   // Create a review for a place
-  createReview = (placeId: string, reviewParams: ReviewProps) => (
-    axios.post('/places/' + placeId + '/reviews', reviewParams)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  );
+  createReview = async (placeId: string, reviewParams: ReviewProps) => {
+    try {
+      const response = await axios.post('/places/' + placeId + '/reviews', reviewParams);
+      console.log(response);
+    } catch(error) {
+        console.error(error);
+    }
+  }
 }
